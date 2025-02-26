df7db3e8047f90af27f9d920bbd21b25c324f26e
akshat <kshatsharmaaa@gmail.com>
2025-02-26 19:40:29 +0530

    creating the dashboard component

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,3 +1,39 @@
 export const Dashboard = () => {
-    return <div className="">Hello world D</div>
+    return (
+    <>
+    <div className="">
+        <div className="">
+            <h1>Expense Tracker</h1>
+            <div className="">
+                <h3>Your Balance</h3>
+                <h2>₹0.00</h2>
+            </div>
+            <div className="">
+                <div className="">
+                    <h4>Income</h4>
+                    <p>₹0.00</p>
+                </div>
+                <div className="">
+                    <h4>Expenses</h4>
+                    <p>₹0.00</p>
+                </div>
+            </div>
+
+            <form action="">
+                <input type="text" placeholder="Description" required />
+                <input type="number" placeholder="Amount" required />
+                <input type="radio" value="expense" id="expense" />
+                <label for="expense">Expense</label>
+                <input type="radio" value="income" id="income" />
+                <label for="income">Income</label>
+                <button>Add Transaction</button>
+            </form>
+        </div>
+    </div>
+
+    <div className="transactions">
+        <h3>Transactions</h3>
+    </div>
+    </>
+)
 }
