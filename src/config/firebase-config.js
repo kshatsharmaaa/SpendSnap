6f962c09313d9420fbbb03ed9ac9ade4babecba9
akshat <kshatsharmaaa@gmail.com>
2025-02-26 19:23:21 +0530

    creating the auth component

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,16 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider} from "firebase/auth"
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZFVPvrXaiUu60qeTFMrnhZN-P9gFzxZ4",
-  authDomain: "spendsnap-2222.firebaseapp.com",
-  projectId: "spendsnap-2222",
-  storageBucket: "spendsnap-2222.firebasestorage.app",
-  messagingSenderId: "847388918759",
-  appId: "1:847388918759:web:cd1d58630ca97351f7b342"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
 
 // firebase login
 // firebase init 
--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,3 +1,34 @@
+
+import {auth, provider} from "../../config/firebase-config"
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate,  } from "react-router-dom";
+// import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+
 export const Auth = () => {
-    return <div className="">Hello world A</div>
+    const navigate = useNavigate();
+    // const { isAuth } = useGetUserInfo();
+
+    const signInWithGoogle = async () => {
+        const results = await signInWithPopup(auth, provider);
+        const authInfo = {
+            userID: results.user.uid,
+            name: results.user.displayName,
+            profilePhoto: results.user.photoURL,
+            isAuth: true,
+        }
+        localStorage.setItem("auth", JSON.stringify(authInfo));
+        navigate("/dashboard");
+    }
+
+    // if (isAuth) {
+    //     return <Navigate to="/dashboard" />;
+    //   }
+
+    return (
+    <div className="login-page">
+        <p>Sign In with Google to continue!</p>
+        <button className=""
+        onClick={signInWithGoogle}>Sign In with Google</button>
+    </div>
+    )
 }
