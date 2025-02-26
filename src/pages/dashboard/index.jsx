5bca45638f9b00e5b19be373a2963ffa20d951bf
akshat <kshatsharmaaa@gmail.com>
2025-02-26 19:50:38 +0530

    added css

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,39 +1,52 @@
+import "../dashboard/style.css";
+
 export const Dashboard = () => {
     return (
-    <>
-    <div className="">
-        <div className="">
-            <h1>Expense Tracker</h1>
-            <div className="">
+        <>
+        <div className="spendsnap-container">
+            <div className="spendsnap-header">
+                <h1>SpendSnap</h1>
+            </div>
+
+            <div className="spendsnap-balance">
                 <h3>Your Balance</h3>
                 <h2>₹0.00</h2>
             </div>
-            <div className="">
-                <div className="">
+
+            <div className="spendsnap-summary">
+                <div>
                     <h4>Income</h4>
                     <p>₹0.00</p>
                 </div>
-                <div className="">
+                <div>
                     <h4>Expenses</h4>
                     <p>₹0.00</p>
                 </div>
             </div>
 
-            <form action="">
+            <form className="spendsnap-form">
                 <input type="text" placeholder="Description" required />
                 <input type="number" placeholder="Amount" required />
-                <input type="radio" value="expense" id="expense" />
-                <label for="expense">Expense</label>
-                <input type="radio" value="income" id="income" />
-                <label for="income">Income</label>
-                <button>Add Transaction</button>
+                <div>
+                    <input type="radio" value="expense" id="expense" />
+                    <label htmlFor="expense">Expense</label>
+                    <input type="radio" value="income" id="income" />
+                    <label htmlFor="income">Income</label>
+                </div>
+                <button type="submit">Add Transaction</button>
             </form>
-        </div>
-    </div>
 
-    <div className="transactions">
-        <h3>Transactions</h3>
-    </div>
-    </>
-)
+            <div className="spendsnap-transactions">
+                <h3>Transactions</h3>
+            </div>
+
+            <footer className="spendsnap-footer">
+                Developed by Akshat Sharma | 
+                <a href="https://github.com/kshatsharmaaa" > GitHub</a>
+            </footer>
+        </div>
+        </>
+    );
 }
+
+
